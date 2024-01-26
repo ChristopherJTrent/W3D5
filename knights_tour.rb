c6ea8b657d1c85326bb757e46ca46361dfa5860f
sam-kim99 <samkim0451@gmail.com>
2024-01-26 17:47:35 -0500

    add instance variable

--- knights_tour.rb
+++ knights_tour.rb
@@ -14,11 +14,15 @@ class KnightPathFinder
 
     def initialize(position)
         @considered_positions = [position]
+        @check_queue = []
     end
     def new_move_positions(pos)
         new_moves = KnightPathFinder.valid_moves(pos)
             .reject{|move| @considered_positions.include?(move)}
         @considered_positions += new_moves
         new_moves
+    end
+    def build_move_tree()
+
     end
 end
